f2bfc2e1bdaad8a005bbfbb7e9ee1b3962e1e5d2
Lisandro Martinez <lisandromartinez@gmail.com>
2020-11-13 18:30:16 -0300

    Initial Commit

---
+++ JobSeeker.Test/TestXX.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+using Xunit;
+
+namespace JobSeeker.Test
+{
+    public class TestXX
+    {
+        [Fact]
+        void XX()
+        {
+            true.Should().BeTrue();
+        }         
+    }
+}
